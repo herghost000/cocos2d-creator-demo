49da8f7ac7233d2acc689948d5b4155deaf5e6e2
ZackAddy <77209302@qq.com>
2020-04-13 18:57:16 +0800

    no message

--- mahjong/assets/scripts/ermj/views/game/ErmjGameMenu.js
+++ mahjong/assets/scripts/ermj/views/game/ErmjGameMenu.js
@@ -24,6 +24,7 @@ cc.Class({
     },
 
     onQuitClick() {
+        this.onMenuClick();
         cc.director.loadScene("ErmjHall");
         EventBus.dispatch(ErmjGameMenuConstant.EventType.JUMP_TO_ERMJ_HALL);
     },
